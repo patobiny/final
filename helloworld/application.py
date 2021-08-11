c80ca1f6725b40fc2bf00b1c430a2492b3b5dc62
EC2 Default User <ec2-user@ip-172-31-33-203.ec2.internal>
2021-08-11 20:03:05 +0000

    dynamo

--- helloworld/application.py
+++ helloworld/application.py
@@ -45,5 +45,70 @@ def uploadImage():
     filename = "%s.jpg" % dt_string
     s3.Bucket(mybucket).upload_fileobj(filobject, filename, ExtraArgs={'ACL': 'public-read', 'ContentType': 'image/jpeg'})
     return {"imgName": filename}
+
+
+
+@application.route('/get_objects' , methods=['GET'])
+def get1():
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('items')
+    resp = table.scan()
+    print(str(resp))
+    return Response(json.dumps(str(resp['Items'])), minetype='application/json', status=200)
+    
+# curl -i http://"localhost:8000/get_object?objectId=1"
+@application.route('/get_object', methods=['GET'])
+def get_obj():
+    objectId = request.args.get('objectId')
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('items')
+    resp = table.get_item(
+    Key={
+        'item_id': objectId,
+    }
+    ) 
+    print(str(resp))
+    return Response(json.dumps(resp['Item']), mimetype='application/json', status=200) 
+    
+# curl -i http://"localhost:8000/set_object?objectId=11-08-2021-18-50-27.jpg&name=amichai&constanse=96"
+@application.route('/set_object', methods=['GET', 'POST'])
+def set_obj():
+    objectId = request.args.get('objectId')
+    name = request.args.get('name')
+    constanse = request.args.get('constanse')
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('items')
+    item={
+        'item_id': objectId,
+        'name':name,
+        'constanse':constanse,
+        
+    }
+     
+    table.put_item(Item=item)
+    return Response(json.dumps(item), mimetype='application/json', status=200)
+    
+@application.route('/set_object/<obj_id>', methods=['POST'])
+def get_temp(obj_id):
+    
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('object')
+    data = request.data
+    data_dict = json.loads(data)
+    object_name = data_dict.get('object_name', 'default')
+    object_confidance = data_dict.get('object_confidance', 'default')
+    
+    item={
+        'object_id' : obj_id,
+        'object_name' : object_name,
+        'object_confidance' : object_confidance
+    }
+    table.put_item(Item=item)
+    
+    
 if __name__ == '__main__':
     flaskrun(application)
+    
+    
+    
+    
